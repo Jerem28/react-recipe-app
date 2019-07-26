d5721199d1ecae1357496ba8998134f761c2cad4
RAKOTOARISOA JEREMY <jeremy.rakotoarisoa@gmail.com>
2019-07-26 17:57:31 +0200

    Application setup (pages, react-router, navbar component)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import SingleRecipe from './pages/SingleRecipe';
+import Default from './pages/Default';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div> Hello  from Recipe App</div>
+      <Router>
+        <main>
+          {/* navbar */}
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/recipes" exact component={Recipes}></Route>
+            <Route path="/recipes/:id" component={SingleRecipe}></Route>
+            <Route component={Default}></Route>            
+          </Switch> 
+        </main>
+      </Router>
     );
   }
 }
